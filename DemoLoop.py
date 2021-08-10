db6eee9a2d29e2110c8591ee5de47b223e182ce3
papasmf1 <papasmf1@gmail.com>
2021-08-10 15:24:07 +0900

    함수 완료

--- DemoLoop.py
+++ DemoLoop.py
@@ -27,4 +27,37 @@ print( list(range(1, 11)) )
 print( list(range(10, 0, -1)) )
 
 
+#리스트 내장(리스트 함축)
+lst = [1,2,3,4,5,6,7,8,9,10]
+result = [i**2 for i in lst if i > 5]
+print(result)
+
+#필터링 함수
+lst = [10, 25, 30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+#함수 정의
+def getBiggerThan20(i):
+    return i>20 
+
+print("---필터링---")
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+#람다 함수
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print(item)
+
+#맵핑함수 
+def add10(i):
+    return i + 10 
+
+#호출(스칼라==>단일값) 
+lst = [1,2,3]
+for i in map(add10, lst):
+    print(i)
 
