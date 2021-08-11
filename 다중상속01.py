4bbe2438db2849ad75e72baacdd01cc44853878a
papasmf1 <papasmf1@gmail.com>
2021-08-11 11:27:01 +0900

    다중 상속

--- 다중상속01.py
+++ 다중상속01.py
@@ -2,19 +2,24 @@
 class Tiger:
     def jump(self):
         print("호랑이 점프")
+    def cry(self):
+        print("호랑이 어흥~~")
 
 class Lion:
     def bite(self):
         print("사자 물어뜯기")
+    def cry(self):
+        print("사자 으르릉~~")
 
-class Liger(Tiger, Lion):
+#다중 상속을 받은 경우 
+class Liger(Lion, Tiger):
     def play(self):
         print("라이거와 놀기")
 
 
 l = Liger()
-l.play()
-l.jump()
-l.bite()
+l.cry() 
+print("내부 힌트:", Liger.__mro__ )
+
 
         
