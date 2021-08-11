7d97730b27391a7ebbfc13222ae386276034c863
papasmf1 <papasmf1@gmail.com>
2021-08-11 10:32:43 +0900

    클래스 이름변경

--- BankAccount.py
+++ BankAccount.py
@@ -1,20 +1,26 @@
 # BankAccount.py
 
 #은행의 계정을 표현한 클래스 
+#모든 멤버가 기본적으로 public 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #클래스 내부에 숨겨달라~~ 
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
 print(account1)
+#print(account1.__balance)
+#외부에서 테스트하는 변경된 이름(백도어)
+#print(account1._BankAccount__balance)
+
--- 참조카운트_증가감소.py
+++ 참조카운트_증가감소.py
@@ -1,13 +1,17 @@
+
 class MyClass:
+    #가장 먼저 초기화를 수행하는 메서드(생성자)
     def __init__(self, value):
         self.Value = value 
-        print("Class is created! Value = ", value)
-        
+        print("Instance is created! Value = ", value)
+    #가장 마지막에 수행되는 메서드(소멸자)    
     def __del__(self):
-        print("Class is deleted!")
+        print("Instance is deleted!")
         
-
+#인스턴스 생성 
 c = MyClass(10)
-c_copy = c
-del c 
-del c_copy
+#del c 
+
+print("전체 코드 실행 종료")
+
+
